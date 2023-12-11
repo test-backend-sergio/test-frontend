33815537e1ccd03103403089e7ea7ab2f8f033ce
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-12-11 19:18:51 -0400

    feat: add request get RecipeListing

--- src/app/Pages/recipe-listing/recipe-listing.component.ts
+++ src/app/Pages/recipe-listing/recipe-listing.component.ts
@@ -1,26 +1,36 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importe o CommonModule
 import { HttpService } from '../../Services/http-client.service';
-
+import { MatTableDataSource } from '@angular/material/table';
+import { IRecipes } from '../../Models/recipes';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from '../../Components/nav/nav.component';
+import { UpdateRecipeComponent } from '../update-recipe/update-recipe.component';
+import { RouterLink } from '@angular/router';
 @Component({
 	selector: 'app-recipe-listing',
 	templateUrl: './recipe-listing.component.html',
-	styleUrls: ['./recipe-listing.component.css']
+	standalone: true,
+	styleUrls: ['./recipe-listing.component.css'],
+	imports: [CommonModule, NavComponent]
 })
 export class RecipeListingComponent {
-	recipes: any[] = [];
+	recipes: IRecipes[] = [];
 
-	constructor(private http: HttpService) {}
+	constructor(private http: HttpService, private uprecipe: UpdateRecipeComponent) {}
 
-	// ngOnInit(): void {
-	// 	this.getRecipes();
+	ngOnInit(): void {
+		this.getRecipes();
+	}
 
-	// }
+	getRecipes() {
+		this.http.getDataRecipe(this.recipes).subscribe((data: any) => {
+			console.log('Buscar Receitas', data);
+			this.recipes = data;
+		});
+	}
 
-  // getRecipes() {
-  //   this.http.getDataRecipe(this.recipes).subscribe((data) => {
-  //     this.recipes = data;
-  //     console.log(data);
-  //   });
-  // }
+	getIdRecipeHtml(Id: any) {
+		this.uprecipe.GetDataUpdatebyId(Id);
+
+	}
 }
