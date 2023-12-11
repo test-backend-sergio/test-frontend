32ddceee142b1f09271f849f25effcf3485819be
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-12-11 19:19:38 -0400

    feat: add request post data Recipe

--- src/app/Pages/recipe-registration/recipe-registration.component.ts
+++ src/app/Pages/recipe-registration/recipe-registration.component.ts
@@ -1,42 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpService } from '../../Services/http-client.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavComponent } from '../../Components/nav/nav.component';
 
 @Component({
 	selector: 'app-recipe-registration',
 	standalone: true,
-	imports: [MatExpansionModule, FormsModule, ReactiveFormsModule],
 	templateUrl: './recipe-registration.component.html',
 	styleUrl: './recipe-registration.component.css',
-
+	imports: [MatExpansionModule, FormsModule, ReactiveFormsModule, NavComponent]
 })
-export class RecipeRegistrationComponent {
-  name: string = '';
-  preparation: string = '';
-  ingredients: string = '';
+export class RecipeRegistrationComponent implements OnInit {
+	name: string = '';
+	preparation: string = '';
+	ingredients: string = '';
 
 	constructor(private http: HttpService) {}
+	ngOnInit(): void {
+		if (localStorage.getItem('token') == null) {
+			window.location.href = 'http://localhost:4200/Login';
+		}
+	}
+
 	PostRecipe() {
 		const [dataRecipeName, dataPreparation, dataIngredients] = [
 			document.getElementById('RecipeName') as HTMLFormElement,
 			document.getElementById('Preparation') as HTMLFormElement,
 			document.getElementById('Ingredients') as HTMLFormElement
 		];
-		const data:any = {
-			name: dataRecipeName['value'],
-      preparation: dataPreparation['value'],
-      ingredients: dataIngredients['value']
-		};
 
-    this.http.postDataRecipe(data);
-		console.log(dataRecipeName['value'], dataPreparation['value'], dataIngredients['value']);
-
-		if (data) {
-      // window.location.href = 'http://localhost:4200/Home';
-      console.log('Receitas enviadas com sucesso!', data);
-		} else {
-			console.log('Erro ao enviar receita!');
+		if (
+			dataRecipeName['value'] == '' ||
+			dataPreparation['value'] == '' ||
+			dataIngredients['value'] == ''
+		) {
+			alert('Preencha todos os campos!');
+			return;
 		}
+		const data: any = {
+			name: dataRecipeName['value'],
+			preparation: dataPreparation['value'],
+			ingredients: dataIngredients['value']
+		};
+		this.http.postDataRecipe(data);
 	}
 }
