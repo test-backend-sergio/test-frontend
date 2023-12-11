9b47f34bb9e4b68f83914b058c28261c13f83a7b
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-12-11 12:03:20 -0400

    fix: problem with sending request resolved for RecipeRegistration

--- src/app/Pages/recipe-registration/recipe-registration.component.ts
+++ src/app/Pages/recipe-registration/recipe-registration.component.ts
@@ -1,46 +1,42 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpService } from '../../Services/http-client.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-recipe-registration',
 	standalone: true,
-	imports: [MatExpansionModule],
+	imports: [MatExpansionModule, FormsModule, ReactiveFormsModule],
 	templateUrl: './recipe-registration.component.html',
-	styleUrl: './recipe-registration.component.css'
+	styleUrl: './recipe-registration.component.css',
+
 })
 export class RecipeRegistrationComponent {
-	adjustTextAreaHeight($event: Event) {
-		throw new Error('Method not implemented.');
-	}
-	texto: any;
+  name: string = '';
+  preparation: string = '';
+  ingredients: string = '';
 
 	constructor(private http: HttpService) {}
-
-	PostRecipe(recipeData: any) {
-		const [dataRecipe, dataPreparation, dataIngredients] = [
+	PostRecipe() {
+		const [dataRecipeName, dataPreparation, dataIngredients] = [
 			document.getElementById('RecipeName') as HTMLFormElement,
-			document.getElementById('Method') as HTMLFormElement,
+			document.getElementById('Preparation') as HTMLFormElement,
 			document.getElementById('Ingredients') as HTMLFormElement
 		];
-		const data = {
-			name: dataRecipe['value'],
+		const data:any = {
+			name: dataRecipeName['value'],
       preparation: dataPreparation['value'],
       ingredients: dataIngredients['value']
 		};
-		// console.log(dataRecipe['value'], dataMethod['value'], dataIngredients['value']);
+
+    this.http.postDataRecipe(data);
+		console.log(dataRecipeName['value'], dataPreparation['value'], dataIngredients['value']);
 
 		if (data) {
       // window.location.href = 'http://localhost:4200/Home';
       console.log('Receitas enviadas com sucesso!', data);
-			this.http.postDataRecipe(recipeData);
 		} else {
 			console.log('Erro ao enviar receita!');
 		}
-	}
-	onChangeTextArea(event: Event): void {
-		const textArea = event.target as HTMLTextAreaElement;
-		textArea.style.height = 'auto';
-		textArea.style.height = textArea.scrollHeight + 'px';
 	}
 }
